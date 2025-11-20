19f75d00c91b61654827307080cc919c169ac0a8
prashanth459g <prashanth459g@gmail.com>
2025-11-20 12:31:47 +0530

    feat: add Android platform and initial configuration for Capacitor project

---
+++ android/app/src/main/java/com/southindianog/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.southindianog.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.southindianog.app',
+  appName: 'SouthIndian OG',
+  webDir: 'dist'
+};
+
+export default config;
